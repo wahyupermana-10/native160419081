3b7a6bd8bdcb5f6d084e35c986d31c17a838101f
Risqi <wahyupermana51@gmail.com>
2021-08-12 16:09:07 +0900

    toast added

--- app/src/main/java/com/ubaya/native160419081/MainActivity.kt
+++ app/src/main/java/com/ubaya/native160419081/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ubaya.native160419081
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
     }
 }
